dc8b2e0e45a19f9c5f3292c3b0922bca1a0925bc
Jacob Ford <j@cobford.com>
2020-03-21 19:08:01 -0400

    Enliven the Hangouts link! + flash it at go time

--- countdown.js
+++ countdown.js
@@ -3,7 +3,18 @@ const element = document.getElementById('door')
 
 function updateCountdown() {
   const msLeft = eventDate - new Date()
-  element.innerHTML = `${msLeft}ms`
+
+  if (msLeft < 0 && Math.floor(msLeft / 10000) % 2) {
+    element.innerHTML = `YOU’RE ${-msLeft}ms LATE` // flash between this…
+  } else if (msLeft < 0) {
+    element.innerHTML = 'meet.google.com/kdq-mnte-qju' // and URL every 10 seconds once started
+  } else if (msLeft < 60000 && Math.floor(msLeft / 100) % 2) {
+    element.innerHTML = 'meet.google.com/kdq-mnte-qju' // strobe URL starting 1 min before
+  } else if (msLeft < 600000 && Math.floor(msLeft / 1000) % 2) {
+    element.innerHTML = 'meet.google.com/kdq-mnte-qju' // flash URL every 1 sec starting 10 mins before
+  } else {
+    element.innerHTML = `${msLeft}ms` // otherwise show ms until showtime
+  }
   requestAnimationFrame(updateCountdown)
 }
 
